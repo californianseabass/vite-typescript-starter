dad719ab3405ff57fe6ccf1a6c2c73b2448735cc
Eric Sebastian <releasetheseabass@gmail.com>
2024-04-15 01:48:54 -0700

    Replace yarn with npm for latest lsp support

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((typescript-mode
-  . ((eval . (let ((project-directory (car (dir-locals-find-file default-directory))))
-               (setq lsp-clients-typescript-server-args `("--tsserver-path" ,(concat project-directory ".yarn/sdks/typescript/bin/tsserver") "--stdio")))))))
